96176f5b1e31c5a10bb89a0cdaf4b9adad077d57
Dan Jordan <dan.jordan@highspeedtraining.co.uk>
2016-04-18 15:38:46 +0100

    added some further type info

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Typed Sound Manager 2
+** warning hugely incomplete **
 The type definition for [`soundmanager2`](https://github.com/scottschiller/SoundManager2)
 
 ## LICENSE
--- index.d.ts
+++ index.d.ts
@@ -3,8 +3,25 @@ interface ISoundManagerOptions {
     url : string;
 }
 
+interface ISoundData {
+    id: string;
+    url: string;
+    autoplay: boolean;
+    volume: number;
+}
+
+declare class SoundManagerSound {
+    playState: number;
+    setVolume(volume: number);
+}
+
 declare class SoundManager {
     setup(options : ISoundManagerOptions);
+    createSound(options :ISoundData);
+    play(id:string);
+    onPosition(id: string, timeMs: number, callback: Function);
+    getSoundById(id:string): SoundManagerSound;
+    reboot(): void;
 }
 
 declare var soundManager : SoundManager;
